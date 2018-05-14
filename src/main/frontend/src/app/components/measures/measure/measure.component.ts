f2bc95d3c394493e6ec998639328bacf529a77eb
AsierG <asiergrc@gmail.com>
2018-05-14 20:51:18 +0200

    pruebas

--- src/main/frontend/src/app/components/measures/measure/measure.component.ts
+++ src/main/frontend/src/app/components/measures/measure/measure.component.ts
@@ -3,6 +3,9 @@ import {ActivatedRoute} from "@angular/router";
 import {MeasuresService} from "../../../services/measures.service";
 import {Measure} from "../measure.model";
 import {NgForm} from "@angular/forms";
+import {NgbDateStruct} from "@ng-bootstrap/ng-bootstrap";
+
+const now = new Date();
 
 @Component({
     selector: 'app-measure',
@@ -11,6 +14,23 @@ import {NgForm} from "@angular/forms";
 export class MeasureComponent implements OnInit {
 
     measure: Measure;
+
+
+    model: NgbDateStruct;
+    date: {year: number, month: number};
+
+    selectToday() {
+        this.model = {year: now.getFullYear(), month: now.getMonth() + 1, day: now.getDate()};
+    }
+
+    fromModel(date: Date): NgbDateStruct {
+        return (date && date.getFullYear) ? {year: date.getFullYear(), month: date.getMonth() + 1, day: date.getDate()} : null;
+    }
+
+    toModel(date: NgbDateStruct): Date {
+        return date ? new Date(date.year, date.month - 1, date.day) : null;
+    }
+
 
     constructor(private activatedRoute: ActivatedRoute,
                 private measuresService: MeasuresService) {
@@ -41,5 +61,7 @@ export class MeasureComponent implements OnInit {
             });
 
     }
+
+
 
 }
