a28d2433c1a42090d701be492f2699332bb333d1
Asier <a.garcia@ariadnagrid.com>
2018-06-14 17:28:58 +0200

    playing with charts.js

--- src/main/frontend/src/app/components/measures/measure-graphics/measure-graphics.component.ts
+++ src/main/frontend/src/app/components/measures/measure-graphics/measure-graphics.component.ts
@@ -1,129 +1,60 @@
-import {Component, Input} from '@angular/core';
+import {Component, ElementRef, Input, OnInit, ViewChild} from '@angular/core';
 import {Measure} from "../measure.model";
-import {WeatherService} from "./weather.service";
-import { Chart } from 'chart.js';
+import {Chart} from 'chart.js';
 
 @Component({
   selector: 'app-measure-graphics',
   templateUrl: './measure-graphics.component.html'
 })
-export class MeasureGraphicsComponent {
+export class MeasureGraphicsComponent implements OnInit{
 
     @Input() measures: Measure[] = [];
 
+    @ViewChild('canvas') canvas: ElementRef;
+
     chart = [];
 
-    constructor(private _weather: WeatherService) {}
+    constructor() {
 
+    }
 
-    ngOnInit() {
-        this._weather.dailyForecast()
-            .subscribe(res => {
+    ngOnInit(): void {
+    }
 
-                let temp_max = res['list'].map(res => res.main.temp_max);
-                let temp_min = res['list'].map(res => res.main.temp_min);
-                let alldates = res['list'].map(res => res.dt);
-
-                let weatherDates = [];
-                alldates.forEach((res) => {
-                    let jsdate = new Date(res * 1000);
-                    weatherDates.push(jsdate.toLocaleTimeString('en', { year: 'numeric', month: 'short', day: 'numeric'}))
-                });
-
-                this.chart = new Chart('canvas', {
-                    type: 'line',
-                    data: {
-                        labels: weatherDates,
-                        datasets: [
-                            {
-                                data: temp_max,
-                                borderColor: '#3cba9f',
-                                fill: false
-                            },
-                            {
-                                data: temp_min,
-                                borderColor: '#ffcc00',
-                                fill: false
-                            },
-                        ]
+    ngAfterViewInit() {
+        let options = {
+            type: 'line',
+            data: {
+                labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+                datasets: [
+                    {
+                        label: '# of Votes',
+                        data: [12, 19, 3, 5, 2, 3],
+                        borderWidth: 1
                     },
-                    options: {
-                        legend: {
-                            display: false
-                        },
-                        scales: {
-                            xAxes: [{
-                                display: true
-                            }],
-                            yAxes: [{
-                                display: true
-                            }]
-                        }
+                    {
+                        label: '# of Points',
+                        data: [7, 11, 5, 8, 3, 7],
+                        borderWidth: 1
                     }
-                })
+                ]
+            },
+            options: {
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            reverse: false
+                        }
+                    }]
+                }
+            }
+        };
 
-            })
+        this.chart = new Chart('canvas', options);
+        // this.chart = new Chart(this.canvas.nativeElement.getContext('2d'), options);
     }
 
 
 
-    // // lineChart
-    // public lineChartData:Array<any> = [
-    //     {data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A'},
-    //     {data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B'},
-    //     {data: [18, 48, 77, 9, 100, 27, 40], label: 'Series C'}
-    // ];
-    // public lineChartLabels:Array<any> = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
-    // public lineChartOptions:any = {
-    //     responsive: true
-    // };
-    // public lineChartColors:Array<any> = [
-    //     { // grey
-    //         backgroundColor: 'rgba(148,159,177,0.2)',
-    //         borderColor: 'rgba(148,159,177,1)',
-    //         pointBackgroundColor: 'rgba(148,159,177,1)',
-    //         pointBorderColor: '#fff',
-    //         pointHoverBackgroundColor: '#fff',
-    //         pointHoverBorderColor: 'rgba(148,159,177,0.8)'
-    //     },
-    //     { // dark grey
-    //         backgroundColor: 'rgba(77,83,96,0.2)',
-    //         borderColor: 'rgba(77,83,96,1)',
-    //         pointBackgroundColor: 'rgba(77,83,96,1)',
-    //         pointBorderColor: '#fff',
-    //         pointHoverBackgroundColor: '#fff',
-    //         pointHoverBorderColor: 'rgba(77,83,96,1)'
-    //     },
-    //     { // grey
-    //         backgroundColor: 'rgba(148,159,177,0.2)',
-    //         borderColor: 'rgba(148,159,177,1)',
-    //         pointBackgroundColor: 'rgba(148,159,177,1)',
-    //         pointBorderColor: '#fff',
-    //         pointHoverBackgroundColor: '#fff',
-    //         pointHoverBorderColor: 'rgba(148,159,177,0.8)'
-    //     }
-    // ];
-    // public lineChartLegend:boolean = true;
-    // public lineChartType:string = 'line';
-    //
-    // public randomize():void {
-    //     let _lineChartData:Array<any> = new Array(this.lineChartData.length);
-    //     for (let i = 0; i < this.lineChartData.length; i++) {
-    //         _lineChartData[i] = {data: new Array(this.lineChartData[i].data.length), label: this.lineChartData[i].label};
-    //         for (let j = 0; j < this.lineChartData[i].data.length; j++) {
-    //             _lineChartData[i].data[j] = Math.floor((Math.random() * 100) + 1);
-    //         }
-    //     }
-    //     this.lineChartData = _lineChartData;
-    // }
-    //
-    // // events
-    // public chartClicked(e:any):void {
-    //     console.log(e);
-    // }
-    //
-    // public chartHovered(e:any):void {
-    //     console.log(e);
-    // }
 
 }
