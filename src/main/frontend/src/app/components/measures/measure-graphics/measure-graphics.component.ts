6a4c7705dfa0b619a850c9512ae157beeb14529d
Asier Garcia <asiergrc@gmail.com>
2018-11-25 19:00:18 +0100

    add dates in js format

--- src/main/frontend/src/app/components/measures/measure-graphics/measure-graphics.component.ts
+++ src/main/frontend/src/app/components/measures/measure-graphics/measure-graphics.component.ts
@@ -1,15 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnChanges, SimpleChanges} from '@angular/core';
+import {Measure} from '../measure.model';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-measure-graphics',
   templateUrl: './measure-graphics.component.html',
   styleUrls: ['./measure-graphics.component.css']
 })
-export class MeasureGraphicsComponent implements OnInit {
+export class MeasureGraphicsComponent implements OnChanges {
 
-  constructor() { }
+    @Input() measures: Measure[];
 
-  ngOnInit() {
-  }
+    public dates: Array<any> = [];
+    public temperatures: Array<Number> = [];
+
+    ngOnChanges(changes: SimpleChanges): void {
+
+        const currentMeasures = <Measure[]> changes.measures.currentValue;
+
+        let prueba: any[] = [];
+
+        if (currentMeasures.length > 0) {
+            console.log('currentMeasures ' + currentMeasures.length );
+            this.dates = currentMeasures
+                .map((measure) => moment(measure.date, 'DD-MM-YYYY HH:mm:ss')
+                    .toDate());
+            this.temperatures = currentMeasures
+                .map((measure) => measure.temperature);
+        }
+
+        console.log('PRUEBA ' + prueba);
+
+    }
+
 
 }
