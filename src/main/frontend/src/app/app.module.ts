f1f55702ed5eaced8af979d0314415bcdcacab44
Asier Garcia <asiergrc@gmail.com>
2018-11-25 18:10:46 +0100

    add ng2-charts

--- src/main/frontend/src/app/app.module.ts
+++ src/main/frontend/src/app/app.module.ts
@@ -1,21 +1,23 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
-//Routes
+// Routes
 import {APP_ROUTING} from './app.routes';
-//Components
+// Components
 import {AppComponent} from './app.component';
 import {NavbarComponent} from './components/shared/navbar/navbar.component';
 import {HomeComponent} from './components/home/home.component';
 import {MeasureListComponent} from './components/measures/measure-list/measure-list.component';
 import {MeasureGraphicsComponent} from './components/measures/measure-graphics/measure-graphics.component';
 import {AboutComponent} from './components/about/about.component';
-import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
-import {HttpClientModule} from "@angular/common/http";
+import {HttpClientModule} from '@angular/common/http';
+import { ChartsModule } from 'ng2-charts';
 
 
-//Services
-import {MeasuresService} from "./services/measures.service";
+
+// Services
+import {MeasuresService} from './services/measures.service';
 import { MeasureComponent } from './components/measures/measure/measure.component';
 import { MeasuresSearchComponent } from './components/measures/measures-search/measures-search.component';
 import { MeasureByDataComponent } from './components/measures/measure-by-data/measure-by-data.component';
@@ -39,6 +41,7 @@ import { MeasureByDataComponent } from './components/measures/measure-by-data/me
         NgbModule.forRoot(),
         HttpClientModule,
         ReactiveFormsModule,
+        ChartsModule,
         APP_ROUTING
     ],
     providers: [MeasuresService],
