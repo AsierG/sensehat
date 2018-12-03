b53f89bcf98e5f7b13dfdd0b97c2f65f3b44ab5d
Asier Garcia <asiergrc@gmail.com>
2018-12-03 20:10:44 +0100

    add pressure and humidity charts

--- src/main/frontend/src/app/components/measures/measure-graphics/measure-graphics.component.ts
+++ src/main/frontend/src/app/components/measures/measure-graphics/measure-graphics.component.ts
@@ -13,32 +13,58 @@ export class MeasureGraphicsComponent implements OnChanges {
 
     @Input() measureInfo: MeasuresInfo;
 
-    private temperaturesMax: Array<number> = [];
-    private temperaturesMin: Array<number> = [];
-    private temperaturesAvg: Array<number> = [];
     private temperaturesChart = [];
+    private pressureChart = [];
+    private humidityChart = [];
 
     ngOnChanges(changes: SimpleChanges): void {
         const currentMeasureInfo = <MeasuresInfo> changes.measureInfo.currentValue;
         if (currentMeasureInfo) {
-            this.temperaturesMax = currentMeasureInfo.temperatureStatistics
-                .map((temperatureStatistics) => Math.round(temperatureStatistics.max * 100) / 100);
-            this.temperaturesMin = currentMeasureInfo.temperatureStatistics
-                .map((temperatureStatistics) => Math.round(temperatureStatistics.min * 100) / 100);
-            this.temperaturesAvg = currentMeasureInfo.temperatureStatistics
-                .map((temperatureStatistics) => Math.round(temperatureStatistics.avg * 100) / 100);
-            this.temperaturesChart = this.getTemperaturesChart(currentMeasureInfo.dates,
-                this.temperaturesMax, this.temperaturesMin, this.temperaturesAvg);
-
+            this.createTemperaturesChart(currentMeasureInfo);
+            this.createPressureChart(currentMeasureInfo);
+            this.createHumidityChart(currentMeasureInfo);
         }
     }
 
+    private createTemperaturesChart(currentMeasureInfo) {
+        const temperaturesMax: Array<number> = currentMeasureInfo.temperatureStatistics
+            .map((temperatureStatistics) => Math.round(temperatureStatistics.max * 100) / 100);
+        const temperaturesMin: Array<number> = currentMeasureInfo.temperatureStatistics
+            .map((temperatureStatistics) => Math.round(temperatureStatistics.min * 100) / 100);
+        const temperaturesAvg: Array<number> = currentMeasureInfo.temperatureStatistics
+            .map((temperatureStatistics) => Math.round(temperatureStatistics.avg * 100) / 100);
+        this.temperaturesChart = this.getTemperaturesChart('Temperatures', 'temperatureCanvas', currentMeasureInfo.dates,
+            temperaturesMax, temperaturesMin, temperaturesAvg);
+    }
 
-    private getTemperaturesChart(weatherDays: Array<string>,
+    private createPressureChart(currentMeasureInfo) {
+        const pressureMax: Array<number> = currentMeasureInfo.pressureStatistics
+            .map((pressureStatistics) => Math.round(pressureStatistics.max * 100) / 100);
+        const pressureMin: Array<number> = currentMeasureInfo.pressureStatistics
+            .map((pressureStatistics) => Math.round(pressureStatistics.min * 100) / 100);
+        const pressureAvg: Array<number> = currentMeasureInfo.pressureStatistics
+            .map((pressureStatistics) => Math.round(pressureStatistics.avg * 100) / 100);
+        this.pressureChart = this.getTemperaturesChart('Pressure', 'pressureCanvas', currentMeasureInfo.dates,
+            pressureMax, pressureMin, pressureAvg);
+    }
+
+    private createHumidityChart(currentMeasureInfo) {
+        const humidityMax: Array<number> = currentMeasureInfo.humidityStatistics
+            .map((humidityStatistics) => Math.round(humidityStatistics.max * 100) / 100);
+        const humidityMin: Array<number> = currentMeasureInfo.humidityStatistics
+            .map((humidityStatistics) => Math.round(humidityStatistics.min * 100) / 100);
+        const humidityAvg: Array<number> = currentMeasureInfo.humidityStatistics
+            .map((humidityStatistics) => Math.round(humidityStatistics.avg * 100) / 100);
+        this.humidityChart = this.getTemperaturesChart('Humidity', 'humidityCanvas', currentMeasureInfo.dates,
+            humidityMax, humidityMin, humidityAvg);
+    }
+
+    private getTemperaturesChart(title: string, canvas: string,
+                                 weatherDays: Array<string>,
                                  maxValues: Array<number>,
                                  meanValues: Array<number>,
                                  minValues: Array<number>) {
-        return new Chart('canvas', {
+        return new Chart(canvas, {
             type: 'bar',
             data: {
                 labels: weatherDays,
@@ -64,6 +90,10 @@ export class MeasureGraphicsComponent implements OnChanges {
                 ]
             },
             options: {
+                title: {
+                    display: true,
+                    text: title
+                },
                 legend: {
                     display: true
                 },
@@ -78,12 +108,20 @@ export class MeasureGraphicsComponent implements OnChanges {
                 scales: {
                     xAxes: [
                         {
-                            display: true
+                            display: true,
+                            scaleLabel: {
+                                display: true,
+                                labelString: 'Date'
+                            }
                         }
                     ],
                     yAxes: [
                         {
                             display: true,
+                            scaleLabel: {
+                                display: true,
+                                labelString: 'Value'
+                            },
                             ticks: {
                                 callback: function (value) {
                                     return new Intl.NumberFormat('es-es',
